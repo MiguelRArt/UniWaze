13976f91da86b9def420a20999b36a39881f5312
MiguelRArt <angel4pride@gmail.com>
2023-05-24 20:30:25 -0500

    xd

---
+++ app3.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.69841878937995,-74.09008026123048], 6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
